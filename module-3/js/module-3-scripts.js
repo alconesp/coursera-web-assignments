56c1f11e8059cfab1ec397887d724daa441c79c1
Alberto Contreras <alconesp@gmail.com>
2016-03-04 22:36:36 +0100

    Module 3 Assigment

---
+++ module-3/js/module-3-scripts.js
@@ -0,0 +1,8 @@
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 60
+    }, 500);
+    return false;
+});
+
+$('body').scrollspy({ target: '#navbar-menu', offset: 70 });
